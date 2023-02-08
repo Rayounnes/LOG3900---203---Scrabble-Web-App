7a0826a2b4a68be0b71757619d0089dde292f3c8
Hady Jaafar <hady.jaafar@polymtl.ca>
2023-02-08 17:17:39 -0500

    change for electron

--- heavy_client/main.js
+++ heavy_client/main.js
@@ -1,36 +1,3 @@
-/* const { app, BrowserWindow } = require('electron')
-
-function createWindow () {
-
-const win = new BrowserWindow({
-
-width: 800,
-
-height: 600,
-
-webPreferences: {
-
-nodeIntegration: true } })
-
-win.loadURL('http://localhost:4200/#/connexion')
-
-win.webContents.openDevTools()
-
-}
-
-app.on('ready', createWindow)
-
-app.on('window-all-closed', () => {
-
-if (process.platform !== 'darwin') {
-
-app.quit() } })
-
-app.on('activate', () => {
-
-if (BrowserWindow.getAllWindows().length === 0) {
-
-createWindow() } }) */
 
 const { app, BrowserWindow } = require('electron');
 
