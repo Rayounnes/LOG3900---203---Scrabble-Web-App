d7d764037806250656b08ef88235e6a903a63918
Rayounnes <rayanzerhouni@hotmail.fr>
2023-03-14 20:09:25 -0400

    popout fonctionne avec le server + handle closing

--- heavy_client/src/app/components/connexion-page/connexion-page.component.ts
+++ heavy_client/src/app/components/connexion-page/connexion-page.component.ts
@@ -1,83 +1,81 @@
 import { Component, OnInit } from '@angular/core';
 import { CommunicationService } from '@app/services/communication.service';
-import { /* ActivatedRoute */ Router } from '@angular/router'; 
-import { MatSnackBar } from '@angular/material/snack-bar'; 
+import { /* ActivatedRoute */ Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { loginInfos } from 'src/constants/login-constants';
 import { ChatSocketClientService } from '@app/services/chat-socket-client.service';
-
+import { AppComponent } from '@app/pages/app/app.component';
 
 @Component({
-  selector: 'app-connexion-page',
-  templateUrl: './connexion-page.component.html',
-  styleUrls: ['./connexion-page.component.scss']
+    selector: 'app-connexion-page',
+    templateUrl: './connexion-page.component.html',
+    styleUrls: ['./connexion-page.component.scss'],
 })
 export class ConnexionPageComponent implements OnInit {
+    connected: boolean = false;
+    accountCreation = false;
+    checkingConnection: boolean = false;
+    username: string = '';
+    password: string = '';
 
-  connected : boolean = false
-  accountCreation = false
-  checkingConnection : boolean = false;
-  username : string = ""
-  password : string = ""
-
-  constructor(private communicationService : CommunicationService, 
-    private _snackBar: MatSnackBar, /* private route: ActivatedRoute, */  private router: Router,
-    public socketService: ChatSocketClientService) {
-      this.connect()
+    constructor(
+        private communicationService: CommunicationService,
+        private _snackBar: MatSnackBar,
+        /* private route: ActivatedRoute, */ private router: Router,
+        public socketService: ChatSocketClientService,
+        private appComponent: AppComponent,
+    ) {
+        this.connect();
     }
 
-  async userConnection() : Promise<void>{
-    
-    let loginInfos : loginInfos = {
-      username : this.username,
-      password : this.password
-    }
-    this.communicationService.userlogin(loginInfos).subscribe((connectionValid) : void =>{
-      if(connectionValid){
-        this.connected = true;
-        this.router.navigate(['home']);
-        this.socketService.send("user-connection",{username :this.username,socketId : this.socketService.socketId});
-      }else{
-        this._snackBar.open("Erreur lors de la connexion. Mauvais nom d'utilisateur et/ou mot de passe ou compte deja connecté. Veuillez recommencer","Fermer")
-      }
-    })
-  
-  }
-
-  createAccount(){
-    let loginInfos : loginInfos = {
-      username : this.username,
-      password : this.password
+    async userConnection(): Promise<void> {
+        let loginInfos: loginInfos = {
+            username: this.username,
+            password: this.password,
+        };
+        this.communicationService.userlogin(loginInfos).subscribe((connectionValid): void => {
+            if (connectionValid) {
+                this.connected = true;
+                this.router.navigate(['home']);
+                this.socketService.send('user-connection', { username: this.username, socketId: this.socketService.socketId });
+                this.appComponent.initiatePopout();
+            } else {
+                this._snackBar.open(
+                    "Erreur lors de la connexion. Mauvais nom d'utilisateur et/ou mot de passe ou compte deja connecté. Veuillez recommencer",
+                    'Fermer',
+                );
+            }
+        });
     }
 
-    this.communicationService.accountCreation(loginInfos).subscribe((accountCreationValid) : void =>{
-      if(accountCreationValid){
-        this.connected = true;
-        this.router.navigate(['home']);
-        this.socketService.send("user-connection",{username :this.username,socketId : this.socketService.socketId});
-      }else{
-        this._snackBar.open("Erreur lors de la création du compte. Nom d'utilisateur deja utilisé. Veuillez recommencer.","Fermer")
-      }
-    })
+    createAccount() {
+        let loginInfos: loginInfos = {
+            username: this.username,
+            password: this.password,
+        };
 
-  }
-
-
-  changeOption(accountCreation : boolean) : void {
-    this.accountCreation = accountCreation
-    this.username = ""
-    this.password = ""
-  }
-
-  
-
-
-  connect() {
-    if (!this.socketService.isSocketAlive()) {
-        this.socketService.connect();
+        this.communicationService.accountCreation(loginInfos).subscribe((accountCreationValid): void => {
+            if (accountCreationValid) {
+                this.connected = true;
+                this.router.navigate(['home']);
+                this.socketService.send('user-connection', { username: this.username, socketId: this.socketService.socketId });
+            } else {
+                this._snackBar.open("Erreur lors de la création du compte. Nom d'utilisateur deja utilisé. Veuillez recommencer.", 'Fermer');
+            }
+        });
     }
-  }
 
-  ngOnInit(): void {
-  }
+    changeOption(accountCreation: boolean): void {
+        this.accountCreation = accountCreation;
+        this.username = '';
+        this.password = '';
+    }
 
+    connect() {
+        if (!this.socketService.isSocketAlive()) {
+            this.socketService.connect();
+        }
+    }
+
+    ngOnInit(): void {}
 }
--- heavy_client/src/app/pages/main-page/main-page.component.ts
+++ heavy_client/src/app/pages/main-page/main-page.component.ts
@@ -1,18 +1,16 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { BestScoresComponent } from '@app/pages/best-scores/best-scores.component';
 import { ChatSocketClientService } from '@app/services/chat-socket-client.service';
-import { PopoutWindowComponent } from 'angular-opinionated-popout-window';
 
 @Component({
     selector: 'app-main-page',
     templateUrl: './main-page.component.html',
     styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent implements AfterViewInit {
-    @ViewChild('popoutWindow') private popoutWindow: PopoutWindowComponent;
+export class MainPageComponent {
     readonly title: string = 'Scrabble';
     h: string;
     message: BehaviorSubject<string> = new BehaviorSubject<string>('');
@@ -23,15 +21,6 @@ export class MainPageComponent implements AfterViewInit {
 
     navModePage(isClassic: boolean) {
         this.router.navigate(['/mode'], { queryParams: { isClassicMode: isClassic } });
-    }
-
-    ngAfterViewInit(): void {
-        this.popoutWindow.wrapperRetainSizeOnPopout = false;
-        this.popoutWindow.whiteIcon = true;
-        this.popoutWindow.innerWrapperStyle = { ['height']: '400px' };
-        this.popoutWindow.popIn();
-        this.popoutWindow.popOut();
-        document.getElementsByClassName('popoutWrapper')[0].setAttribute('style', 'display : none');
     }
 
     popUp() {
