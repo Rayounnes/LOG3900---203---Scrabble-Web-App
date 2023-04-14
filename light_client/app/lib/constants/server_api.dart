eb404a04660016ef4f412054c38f722109302bda
RayanBouteldja <70912595+RayanBouteldja@users.noreply.github.com>
2023-04-14 16:14:00 -0400

    aws server

--- light_client/app/lib/constants/server_api.dart
+++ light_client/app/lib/constants/server_api.dart
@@ -1,4 +1,5 @@
 class ApiConstants {
-  static String baseUrl = 'http://localhost:3000';
+  static String baseUrl =
+      'http://ec2-99-79-43-177.ca-central-1.compute.amazonaws.com:3000'; // 'http://localhost:3000';
   // static String baseUrl = 'http://ec2-99-79-43-177.ca-central-1.compute.amazonaws.com:3000';
 }
